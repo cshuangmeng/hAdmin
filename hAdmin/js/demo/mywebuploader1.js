2dc3b6c42575e96bd5e497d41ec143efb46e943e
黄盟 <huangmeng@haotang365.com.cn>
2017-10-11 20:38:28 +0800

    1、哟哟

---
+++ hAdmin/js/demo/mywebuploader1.js
@@ -0,0 +1,31 @@
+$(function(){
+	alert(1);
+});
+
+var uploader = WebUploader.create({
+    // swf文件路径
+    swf: 'js/plugins/webuploader/js/Uploader.swf',
+
+    // 文件接收服务端。
+    server: 'http://webuploader.duapp.com/server/fileupload.php',
+
+    // 选择文件的按钮。可选。
+    // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+    pick: '#picker',
+
+    // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
+    resize: false
+});
+
+uploader.on( 'uploadSuccess', function( file ) {
+    $( '#'+file.id ).find('p.state').text('已上传');
+});
+
+uploader.on( 'uploadError', function( file ) {
+    $( '#'+file.id ).find('p.state').text('上传出错');
+});
+
+uploader.on( 'uploadComplete', function( file ) {
+    $( '#'+file.id ).find('.progress').fadeOut();
+});
+
